673ac7133a1b0514b3280264b6501b2ddb080b29
Ubong Jacob <jacobubong12@gmail.com>
2022-09-01 15:30:07 +0100

    Initial setup and addition of basic components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='headerList'>
+        <div className='headerListItem'>
+          <FontAwesomeIcon icon={faBed} />
+          <span>Stays</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
